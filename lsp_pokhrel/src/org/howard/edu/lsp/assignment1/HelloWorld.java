77707f7535727b2f44ff00d00d9d6c167d9ce0e3
Aniruddha18-SPD <72161591+Aniruddha18-SPD@users.noreply.github.com>
2022-08-30 13:13:06 -0400

    First commit for assignment

---
+++ lsp_pokhrel/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Aniruddha Pokhrel
+ * 
+ * @author Anirudd
+ *
+ *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
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello LSP!!!!!!");
+
+	}
+
+}
